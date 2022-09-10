d6d3232598950e96d181596c62b729e738223648
Lavan Sumanan <lavan.sumanan@gmail.com>
2022-09-10 19:44:13 -0400

    initial setup + examples

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import { StyledMainTitle } from './Headings'
+
+const headers = []
+
+export default function Header() {
+    return (
+        <div>
+            <StyledMainTitle isBlue={true} myColor="red">Header</StyledMainTitle>
+        </div>
+    )
+}
---
+++ components/Headings.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const StyledMainTitle = styled.h1`
+    font-size: 50px;
+`
+
+export const StyledSubTitle = styled.h2`
+    font-size: 15px;
+`
