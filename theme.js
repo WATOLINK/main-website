aad2d02795fbbd9db3a1fb46a297e46360a033ba
Lavan Sumanan <lavan.sumanan@gmail.com>
2022-09-11 14:48:49 -0400

    added accent colour + fixed font weights in theme

--- theme.js
+++ theme.js
@@ -1,26 +1,27 @@
 // TODO: setup fonts, colours, sizes
 
 const fonts = {
-    nav: `normal 500 20px 'Poppins'`,
-    footer: `normal 600 20px 'Poppins'`,
+    nav: `normal 600 20px 'Poppins', sans-serif`,
+    footer: `normal 600 20px 'Poppins', sans-serif`,
 
-    small20bolder: `normal 700 20px 'Poppins'`,
+    small20bolder: `normal 700 20px 'Poppins', sans-serif`,
 
-    medium24: `normal 500 24px 'Poppins'`,
-    medium24bold: `normal 700 24px 'Poppins'`,
+    medium24: `normal 500 24px 'Poppins', sans-serif`,
+    medium24bold: `normal 700 24px 'Poppins', sans-serif`,
 
-    medium28: `normal 500 28px 'Poppins'`,
-    medium28bold: `normal 700 28px 'Poppins'`,
+    medium28: `normal 500 28px 'Poppins', sans-serif`,
+    medium28bold: `normal 700 28px 'Poppins', sans-serif`,
 
-    subheading: `normal 500 30px 'Poppins'`,
-    heading: `normal 700 45px 'Poppins'`,
-    largeheading: `normal 600 72px 'Poppins'`,
+    subheading: `normal 600 30px 'Poppins', sans-serif`,
+    heading: `normal 700 45px 'Poppins', sans-serif`,
+    largeheading: `normal 700 72px 'Poppins', sans-serif`,
 }
 
 const colors = {
     white: '#FFFFFF',
     black: '#000000',
     blue: '#0088FF',
+    accent: '#0088FF',
     lightpurple: '#BDA6D0',
     green: '#BBE1B0',
     lightblue: '#92C3FE',
