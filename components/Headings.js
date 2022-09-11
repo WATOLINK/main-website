3cb617a18a1c510af390e1f9460fa0476f8a55dd
Lavan Sumanan <lavan.sumanan@gmail.com>
2022-09-11 12:48:12 -0400

    add global themes

--- components/Headings.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const StyledMainTitle = styled.h1`
-    font-size: 50px;
-`
-
-export const StyledSubTitle = styled.h2`
-    font-size: 15px;
-`
--- theme.js
+++ theme.js
@@ -1,3 +1,43 @@
 // TODO: setup fonts, colours, sizes
 
-const fonts = {}
+const fonts = {
+    small20: `normal 500 20px 'Poppins'`,
+    header: small20,
+
+    small20bold: `normal 600 20px 'Poppins'`,
+    footer: small20bold,
+
+    small20bolder: `normal 700 20px 'Poppins'`,
+
+    medium24: `normal 500 24px 'Poppins'`,
+    medium24bold: `normal 700 24px 'Poppins'`,
+
+    medium28: `normal 500 28px 'Poppins'`,
+    medium28bold: `normal 700 28px 'Poppins'`,
+
+    medium30: `normal 500 30px 'Poppins'`,
+    subheading: medium30,
+
+    large45bold: `normal 700 45px 'Poppins'`,
+    heading: large45bold,
+
+    large72: `normal 600 72px 'Poppins'`,
+    largeheading: large72,
+}
+
+const colors = {
+    black: '#000000',
+    blue: '#0088FF',
+    lightpurple: '#BDA6D0',
+    green: '#BBE1B0',
+    lightblue: '#92C3FE',
+    yellow: '#FBFBB5',
+    greyblue: '#B6C2E0',
+    pink: '#FBE1E0',
+    grey: '#949494',
+}
+
+export default theme = {
+    colors,
+    fonts,
+}
