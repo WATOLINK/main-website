d67afd1440b5b17686c79a7b61a269ab28341463
Lavan Sumanan <lavan.sumanan@gmail.com>
2022-09-11 14:10:59 -0400

    fix redirect (go from /about to / instead of vice versa)

--- middleware.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-export async function middleware(req) {
-    const { pathname } = req.nextUrl
-    if (pathname == '/') {
-        return NextResponse.redirect('/about')
-    }
-    return NextResponse.next()
-}
