20526a38d8769146c05ea0d8919ac51f1601469e
Thomson Tong <tomtong2006@gmail.com>
2026-02-09 20:40:00 -0500

    added project leads

---
+++ constants/members/mindflex.js
@@ -0,0 +1,13 @@
+import Aidan from '../../public/members/mindflex/Aidan.png'
+
+const MINDFLEX = [
+    {
+        name: 'AIDAN KIRWIN',
+        position: 'Admin',
+        program: 'Biomedical Engineering',
+        icon: Aidan,
+        lead: true,
+    },
+]
+
+export default MINDFLEX
---
+++ constants/members/transcription.js
@@ -0,0 +1,22 @@
+import Steven from '../../public/members/transcription/Steven.png'
+import Fahmi from '../../public/members/transcription/Fahmi.png'
+
+const TRANSCRIPTION = [
+    {
+        name: 'STEVEN LI',
+        position: 'Admin',
+        program: 'CS and Business Administration',
+        icon: Steven,
+        lead: true,
+    },
+    {
+        name: 'FAHMI OMER',
+        position: 'Admin',
+        program: 'Computer Science',
+        icon: Fahmi,
+        lead: true,
+  },
+    
+]
+
+export default TRANSCRIPTION
